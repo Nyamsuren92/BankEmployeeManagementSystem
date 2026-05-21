874e7ce22ca1d7ae3509e9231a6ffb3b442e3910
Nyamsuren92 <2025412@student.cct.ie>
2026-05-21 20:11:16 +0100

    Added enum classes

---
+++ src/main/java/CA_2/ManagerType.java
@@ -0,0 +1,18 @@
+package CA_2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author nyamka.t
+ */
+public enum ManagerType {
+
+    HEAD_MANAGER,
+    ASSISTANT_MANAGER,
+    TEAM_LEADER
+
+}
